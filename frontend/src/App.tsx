85893ded0da9916bb3410a1af5d74c03263fda02
programmer8760 <programmer8760@gmail.com>
2025-11-24 01:16:31 +0300

    add loading screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,18 @@ import ResultsPage from './pages/ResultsPage';
 import { useState, useEffect } from 'react';
 import { Parse } from '../wailsjs/go/app/App';
 import { types } from '../wailsjs/go/models';
+import { EventsOn } from '../wailsjs/runtime/runtime';
 
 function App() {
   const [input, setInput] = useState<string>("");
   const [parserResult, setParserResult] = useState<types.ParserResult | null>(null);
+  const [ready, setReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    EventsOn('parserReady', () => {
+      setReady(true);
+    });
+  }, []);
 
   useEffect(() => {
     if (!input) {
@@ -20,7 +28,11 @@ function App() {
     run();
   }, [input]);
 
-  if (input === "") {
+  if (ready === false) {
+    return(
+      <p className='text-6xl text-center mt-32'>Парсер загружается...</p>
+    );
+  } else if (input === "") {
     return (
       <InputPage onSubmit={(v: string) => setInput(v)} />
     );
