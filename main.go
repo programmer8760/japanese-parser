82c78dbdd24f90ab472c9443c02a095297732425
programmer8760 <programmer8760@gmail.com>
2025-12-20 00:29:30 +0300

    change default window name (also change windows icon)

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:            "wails-events",
+		Title:            "JP-411",
 		Width:            1024,
 		Height:           768,
 		MinWidth:         800,
